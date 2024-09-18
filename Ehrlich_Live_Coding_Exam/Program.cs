4202e5f20f7e74e8b8e9c58b37eff02bfc24bec3
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-09-18 17:11:55 +0800

    Palindrome coding challenge

--- Ehrlich_Live_Coding_Exam/Program.cs
+++ Ehrlich_Live_Coding_Exam/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    Console.Write("Input a Word:");
+
+    var input = Console.ReadLine();
+
+    bool isPalindrome = true;
+
+    for (var i = 0; i <= input!.Length / 2; i++)
+    {
+        if (!(input[i] == input[input.Length - (i + 1)]))
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    Console.WriteLine(isPalindrome ? "Palindrome" : "Not Palindrome");
+    Console.WriteLine("~~~~~~~~~~~~~~~");
+}
